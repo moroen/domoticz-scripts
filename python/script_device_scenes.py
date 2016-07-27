f66999afd5272cd3e51cc6fa1511c1ac38addfc9
Moroen <moroen@gmail.com>
2016-07-28 01:37:10 +0200

    Disabled old lua-scripts, updated test triggers

--- python/script_device_scenes.py
+++ python/script_device_scenes.py
@@ -11,31 +11,38 @@ def check_conditions(conditions):
     res = True
 
     for a in conditions:
-        # print s_value, a["opr"], a["val"]
-        # print operators[a["opr"]](int(s_value), a["val"])
-        sensorDevice = domoticz.devices[a["sensor"]]
-        domoticz.log("Scenes - sensor: ", sensorDevice.name, "switch_type: ", sensorDevice.switch_type, "n_value: ", sensorDevice.n_value, " s_value: ", sensorDevice.s_value)
+        if "sensor" in a and "val" in a:
+            sensorDevice = domoticz.devices[a["sensor"]]
 
-        if sensorDevice.switch_type == 0:
-            # On/Off Switch
-            res &= True if operators[a["val"]] == sensorDevice.n_value else False
-            domoticz.log("On/Off: ", a, res)
+            domoticz.log("Conditional scenes: sensor: ", sensorDevice.name, "s_value: ", sensorDevice.s_value, "n_value:", sensorDevice.n_value)
 
-        elif sensorDevice.switch_type == 7:
-            # MultiLevel
-            res &= operators[a["opr"]](int(sensorDevice.s_value), int(a["val"]))
-            domoticz.log("Multilevel: ", a, res)
+            if sensorDevice.switch_type == 0:
+                # On/Off Switch
+                res &= True if operators[a["val"]] == sensorDevice.n_value else False
+            elif sensorDevice.switch_type == 7:
+                # MultiLevel
+                if "opr" in a:
+                    res &= operators[a["opr"]](int(sensorDevice.s_value), int(a["val"]))
+                else:
+                    domoticz.log("Condtional scenes: Missing required key (opr)")
+                    res = False
+        else:
+            domoticz.log("Conditional scenes: Missing required keys (sensor, val)")
+            res = False
 
     return res
 
-sceneTriggers = {"Test": [
-    {"scene": "Test_Low",
-        "conditions": [{"sensor": "Test", "val": "Off"}, {"sensor": "Stue - Hjørnelampe", "opr": "gt", "val": 50}]},
-    {"scene": "Test",
-        "conditions": [{"sensor": "Test", "val": "Off"}, {"sensor": "Stue - Hjørnelampe", "opr": "le", "val": 50}]}
+# Define triggers
+# Due to using impulse-triggers, only execute scene when the trigger is Off
+
+sceneTriggers = {"$Ute - Platting - Switch 1": [
+    {"scene": "Stue - Dempet",
+        "conditions": [{"sensor": "$Ute - Platting - Switch 1", "val": "Off"}, {"sensor": "Stue 2 - Tak", "opr": "ge", "val": 50}]},
+    {"scene": "Stue - Av",
+        "conditions": [{"sensor": "$Ute - Platting - Switch 1", "val": "Off"}, {"sensor": "Stue 2 - Tak", "opr": "le", "val": 50}]}
 ]}
 
-# print sceneTriggers
+print sceneTriggers
 
 triggerDev = domoticz.changed_device
 
@@ -45,4 +52,3 @@ if triggerDev.name in sceneTriggers:
 
         if check_conditions(conditions=a["conditions"]):
             domoticz.command(name="Scene:" + a["scene"], action="On", file=__file__)
-            
