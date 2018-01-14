0a16cce12a93d543ca4eba3ac86b62146b345b68
Moroen <moroen@gmail.com>
2018-01-14 20:46:43 +0100

    Cleanup

--- python/not_working/script_device_broadcast.py
+++
@@ -1,9 +0,0 @@
-import domoticz
-import udp as bc
-
-if 0:
-    domoticz.log(
-        "Python: Sending DeviceChange for device: ",
-        changed_device_name)
-
-    bc.send("Domoticz:DeviceChange:" + str(changed_device.id))
